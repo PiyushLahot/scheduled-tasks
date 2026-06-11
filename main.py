0a731e0dacfec994a97fabfa7e2383cabc93432e
Piyush Lahot <lahot.piyush@gmail.com>
2026-06-11 17:49:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+##################### Extra Hard Starting Project ######################
+
+# 1. Update the birthdays.csv
+
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+
 import pandas as pd
 import smtplib
 import random as rnd
@@ -11,6 +22,7 @@ df=pd.read_csv('birthdays.csv')
 dict_df=df.to_dict(orient='records')
 letter_temp=[]
 
+print(dict_df)
 now=dt.datetime.now()
 with open("./letter_templates/letter_1.txt") as f:
     letter = f.read()
